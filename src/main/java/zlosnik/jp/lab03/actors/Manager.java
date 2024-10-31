4601d07776daf30063b8e58dc07faced6636166b
Salad109 <szolek1337@gmail.com>
2024-11-01 00:07:48 +0100

    add tenant

--- src/main/java/zlosnik/jp/lab03/actors/Manager.java
+++ src/main/java/zlosnik/jp/lab03/actors/Manager.java
@@ -3,6 +3,8 @@ package zlosnik.jp.lab03.actors;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Manager {
     private static final String ORDERS_PATH = "orders.txt";
@@ -15,6 +17,18 @@ public class Manager {
             bw.newLine();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void addTenant(int id, String street) {
+        List<String> files = new ArrayList<>();
+        files.add("heat.txt");
+        files.add("readings.txt");
+        files.add("rents.txt");
+        // files.add("tenants.txt"); // TODO OSOBNO
+
+        for (String file : files) {
+            List<String> lines = DatabaseManager.readAllLines(file);
         }
     }
 }
